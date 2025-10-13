57f268f77a86d7f8db0fc79e20e8841768880255
sandeshpoudel <sandeshpoudel@live.com>
2025-10-13 08:50:49 +0545

    dash to frontend linkrd

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    {{ __("You're logged in!") }} <a href="/">Click here to view Frontend </a>
                 </div>
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                    <a href="articles"> {{ __("Go To Article") }}</a>
