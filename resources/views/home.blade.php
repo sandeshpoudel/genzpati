c0f474060a5fcc4ea0481db8c329a0dc21650c43
sandeshpoudel <sandeshpoudel@live.com>
2025-10-13 15:04:03 +0545

    fixed default image not showing issue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -204,15 +204,18 @@
                     @if($article->featured_image)
                         <img src="{{ asset('storage/' . $article->featured_image) }}"
                              alt="{{ $article->title }}"
-                             class="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-700">
+                             class="w-full h-full object-cover transform group-hover:scale-110 transition-transform duration-700"/>
                     @else
                         <div class="w-full h-full flex items-center justify-center bg-gradient-to-br from-blue-100 to-purple-100">
-                            <svg class="w-16 h-16 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <img src="{{ asset('images/default-article.jpg') }}" 
+                                 alt="Default Image" 
+                                 class="opacity-50"/>
+                            {{-- <svg class="w-16 h-16 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"/>
-                            </svg>
+                            </svg> --}}
                         </div>
                     @endif
-                    
+
                     <!-- Gradient Overlay -->
                     <div class="absolute inset-0 bg-gradient-to-t from-black/60 via-transparent to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
                     
