f01d6f431cbcedfde309c280c95e6225c91bf1be
sandeshpoudel <sandeshpoudel@live.com>
2025-12-10 17:37:45 +0545

    Added Map in contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,5 +13,6 @@
             </div>
         </div>
     </div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d5325.1881517820875!2d84.42940690679032!3d27.689577463981394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sbirendra%20campus!5e1!3m2!1sen!2snp!4v1765367361819!5m2!1sen!2snp" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
 </x-app-layout>
